181e75d9f23b84b7cb2868eed6ea826d5df4daf8
Keijiro Takahashi <keijiro@ZX.local>
2015-10-24 21:21:00 +0900

    initial commit

---
+++ Assets/Kino/Bloom/Bloom.cs
@@ -0,0 +1,129 @@
+﻿//
+// KinoBloom - Bloom effect
+//
+// Copyright (C) 2015 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using UnityEngine;
+
+namespace Kino
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    [AddComponentMenu("Kino Image Effects/Bloom")]
+    public class Bloom : MonoBehaviour
+    {
+        #region Public Properties
+
+        // Radius
+        [SerializeField, Range(2, 40)]
+        float _radius = 10.0f;
+
+        public float radius {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        // Threshold
+        [SerializeField, Range(0, 2)]
+        float _threshold = 0.5f;
+
+        public float threshold {
+            get { return _threshold; }
+            set { _threshold = value; }
+        }
+
+        // Intensity
+        [SerializeField, Range(0, 1.0f)]
+        float _intensity = 0.2f;
+
+        public float intensity {
+            get { return _intensity; }
+            set { _intensity = value; }
+        }
+
+        #endregion
+
+        #region Private Properties
+
+        [SerializeField] Shader _shader;
+
+        Material _material;
+
+        #endregion
+
+        #region MonoBehaviour Functions
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            // Always use an HDR texture.
+            var fmt = RenderTextureFormat.DefaultHDR;
+
+            // Width/height of the blur buffer
+            var blurHeight = (int)(Screen.width / _radius);
+            var blurWidth = blurHeight * Screen.width / Screen.height;
+
+            // Set up the material object.
+            if (_material == null)
+            {
+                _material = new Material(_shader);
+                _material.hideFlags = HideFlags.DontSave;
+            }
+
+            _material.SetFloat("_Threshold", _threshold);
+            _material.SetFloat("_Intensity", _intensity);
+
+            // Shrink the source image with the quarter downsampler.
+            RenderTexture rt = source;
+            while (rt.height > blurHeight * 4)
+            {
+                var rt_next = RenderTexture.GetTemporary(rt.width / 4, rt.height / 4, 0, fmt);
+                Graphics.Blit(rt, rt_next, _material, 0);
+                if (rt != source) RenderTexture.ReleaseTemporary(rt);
+                rt = rt_next;
+            }
+
+            // Double blur buffer
+            var rt1 = RenderTexture.GetTemporary(blurWidth, blurHeight, 0, fmt);
+            var rt2 = RenderTexture.GetTemporary(blurWidth, blurHeight, 0, fmt);
+
+            // Shrink to the size of the blur buffer with the downsampler.
+            // This time it applies the threshold function.
+            Graphics.Blit(rt, rt1, _material, 1);
+            if (rt != source) RenderTexture.ReleaseTemporary(rt);
+
+            // Apply the separable box filter repeatedly.
+            for (var i = 0; i < 4; i++)
+            {
+                Graphics.Blit(rt1, rt2, _material, 2); // horizontal
+                Graphics.Blit(rt2, rt1, _material, 3); // vertical
+            }
+
+            // Compositing
+            _material.SetTexture("_BlurTex", rt1);
+            Graphics.Blit(source, destination, _material, 4);
+
+            // Release the blur buffers.
+            RenderTexture.ReleaseTemporary(rt1);
+            RenderTexture.ReleaseTemporary(rt2);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Kino/Bloom/Shader/Bloom.shader
@@ -0,0 +1,151 @@
+﻿//
+// KinoBloom - Bloom effect
+//
+// Copyright (C) 2015 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+Shader "Hidden/Kino/Bloom"
+{
+    Properties
+    {
+        _MainTex("-", 2D) = "" {}
+        _BlurTex("-", 2D) = "" {}
+    }
+
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    float2 _MainTex_TexelSize;
+
+    sampler2D _BlurTex;
+    float _Threshold;
+    float _Intensity;
+
+    // Quarter downsampler
+    half4 frag_downsample(v2f_img i) : SV_Target
+    {
+        float4 d = _MainTex_TexelSize.xyxy * float4(1, 1, -1, -1);
+        half4 s;
+        s  = tex2D(_MainTex, i.uv + d.xy);
+        s += tex2D(_MainTex, i.uv + d.xw);
+        s += tex2D(_MainTex, i.uv + d.zy);
+        s += tex2D(_MainTex, i.uv + d.zw);
+        return s * 0.25;
+    }
+
+    half4 frag_downsample_last(v2f_img i) : SV_Target
+    {
+        half4 s = frag_downsample(i);
+        half l = Luminance(s.rgb);
+        return s * smoothstep(_Threshold, _Threshold * 1.5, l);
+    }
+
+    // 13-tap box filter with linear sampling
+    half4 box_filter(float2 uv, float2 stride)
+    {
+        half4 s = tex2D(_MainTex, uv) / 2;
+
+        float2 d1 = stride * 1.5;
+        s += tex2D(_MainTex, uv + d1);
+        s += tex2D(_MainTex, uv - d1);
+
+        float2 d2 = stride * 3.5;
+        s += tex2D(_MainTex, uv + d2);
+        s += tex2D(_MainTex, uv - d2);
+
+        float2 d3 = stride * 5.5;
+        s += tex2D(_MainTex, uv + d3);
+        s += tex2D(_MainTex, uv - d3);
+
+        return s * 2 / 13;
+    }
+
+    // Separable blur filters
+    half4 frag_blur_h(v2f_img i) : SV_Target
+    {
+        return box_filter(i.uv, float2(_MainTex_TexelSize.x, 0));
+    }
+
+    half4 frag_blur_v(v2f_img i) : SV_Target
+    {
+        return box_filter(i.uv, float2(0, _MainTex_TexelSize.y));
+    }
+
+    // Composite function
+    half4 frag_composite(v2f_img i) : SV_Target
+    {
+        half4 cs = tex2D(_MainTex, i.uv);
+        half3 c1 = LinearToGammaSpace(cs.rgb);
+        half3 c2 = LinearToGammaSpace(tex2D(_BlurTex, i.uv).rgb);
+        half3 co = GammaToLinearSpace(c1 + c2 * _Intensity);
+        return half4(co, cs.a);
+    }
+
+    ENDCG
+    SubShader
+    {
+        Pass
+        {
+            ZTest Always Cull Off ZWrite Off
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag_downsample
+            #pragma target 3.0
+            ENDCG
+        }
+        Pass
+        {
+            ZTest Always Cull Off ZWrite Off
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag_downsample_last
+            #pragma target 3.0
+            ENDCG
+        }
+        Pass
+        {
+            ZTest Always Cull Off ZWrite Off
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag_blur_h
+            #pragma target 3.0
+            ENDCG
+        }
+        Pass
+        {
+            ZTest Always Cull Off ZWrite Off
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag_blur_v
+            #pragma target 3.0
+            ENDCG
+        }
+        Pass
+        {
+            ZTest Always Cull Off ZWrite Off
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag_composite
+            #pragma target 3.0
+            ENDCG
+        }
+    }
+}
