b63825c44ba28189673f37f9df57271ed43ec4a8
Keijiro Takahashi <keijiro@ZX.local>
2015-10-25 11:16:12 +0900

    add unitypackage file

---
+++ Assets/Editor/PackageTool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PackageTool
+{
+    [MenuItem("Package/Update Package")]
+    static void UpdatePackage()
+    {
+        AssetDatabase.ExportPackage("Assets/Kino", "KinoBloom.unitypackage", ExportPackageOptions.Recurse);
+    }
+}
