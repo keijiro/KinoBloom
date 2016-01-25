b554873c0f4a23e4e466be12b4259f1a737da476
Keijiro Takahashi <keijiro@Ultra.jt.unity3d.com>
2016-01-25 17:13:18 +0900

    Update unitypackage file
    - Updated the copyright note.
    - Updated the unitypackage file.

--- Assets/Kino/Bloom/Editor/BloomEditor.cs
+++ Assets/Kino/Bloom/Editor/BloomEditor.cs
@@ -1,7 +1,7 @@
 //
-// KinoBloom - Bloom effect
+// Kino/Bloom v2 - Bloom filter for Unity
 //
-// Copyright (C) 2015 Keijiro Takahashi
+// Copyright (C) 2015, 2016 Keijiro Takahashi
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
