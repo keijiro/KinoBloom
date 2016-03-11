3b5b19afd0e38f18c18e56959367af2337d4d77d
Keijiro Takahashi <keijiro@Ultra.jt.unity3d.com>
2016-03-11 11:58:55 +0900

    Refactored the graph code

--- Assets/Kino/Bloom/Editor/BloomEditor.cs
+++ Assets/Kino/Bloom/Editor/BloomEditor.cs
@@ -30,30 +30,27 @@ namespace Kino
     [CustomEditor(typeof(Bloom))]
     public class BloomEditor : Editor
     {
+        #region Property fields
+
         SerializedProperty _threshold;
         SerializedProperty _softKnee;
-        SerializedProperty _radius;
         SerializedProperty _intensity;
+        SerializedProperty _radius;
         SerializedProperty _highQuality;
         SerializedProperty _antiFlicker;
-
-        const int _curveResolution = 128;
-        Vector3[] _rectVertices;
-        Vector3[] _curveVertices;
-        Vector3[] _lineVertices;
 
         void OnEnable()
         {
             _threshold = serializedObject.FindProperty("_threshold");
             _softKnee = serializedObject.FindProperty("_softKnee");
-            _radius = serializedObject.FindProperty("_radius");
             _intensity = serializedObject.FindProperty("_intensity");
+            _radius = serializedObject.FindProperty("_radius");
             _highQuality = serializedObject.FindProperty("_highQuality");
             _antiFlicker = serializedObject.FindProperty("_antiFlicker");
 
             _rectVertices = new Vector3[4];
-            _curveVertices = new Vector3[_curveResolution];
             _lineVertices = new Vector3[2];
+            _curveVertices = new Vector3[_curveResolution];
         }
 
         public override void OnInspectorGUI()
@@ -68,23 +65,35 @@ namespace Kino
 
             EditorGUILayout.PropertyField(_threshold);
             EditorGUILayout.PropertyField(_softKnee);
-            EditorGUILayout.PropertyField(_radius);
             EditorGUILayout.PropertyField(_intensity);
+            EditorGUILayout.PropertyField(_radius);
             EditorGUILayout.PropertyField(_highQuality);
             EditorGUILayout.PropertyField(_antiFlicker);
 
             serializedObject.ApplyModifiedProperties();
         }
 
+        #endregion
+
+        #region Graph drawing
+
+        const int _curveResolution = 64;
+        const float _graphRangeX = 5;
+        const float _graphRangeY = 1.5f;
+
+        Vector3[] _rectVertices;
+        Vector3[] _lineVertices;
+        Vector3[] _curveVertices;
+
         void DrawGraph(Bloom bloom)
         {
-            var rect = GUILayoutUtility.GetRect(128, 64+16);
+            var rect = GUILayoutUtility.GetRect(128, 80);
 
             // Parameters
             var threshold = bloom.threshold;
             var knee = threshold * bloom.softKnee;
-            var param1 = 0.25f / Mathf.Max(knee, 1e-5f);
-            var param2 = threshold - knee;
+            var coeff1 = 0.25f / Mathf.Max(knee, 1e-5f);
+            var coeff2 = threshold - knee;
 
             // Background
             _rectVertices[0] = new Vector3(rect.x, rect.y);
@@ -96,60 +105,64 @@ namespace Kino
                 _rectVertices, Color.white * 0.1f, Color.white * 0.4f
             );
 
-            // Knee range
+            // Soft-knee range
             _rectVertices[0] = PointInRect(rect, threshold - knee, 0);
             _rectVertices[1] = PointInRect(rect, threshold + knee, 0);
-            _rectVertices[2] = PointInRect(rect, threshold + knee, 2);
-            _rectVertices[3] = PointInRect(rect, threshold - knee, 2);
+            _rectVertices[2] = PointInRect(rect, threshold + knee, _graphRangeY);
+            _rectVertices[3] = PointInRect(rect, threshold - knee, _graphRangeY);
 
             Handles.DrawSolidRectangleWithOutline(
                 _rectVertices, Color.white * 0.25f, Color.clear
             );
 
-            // Vertical lines
+            // Horizontal lines
+            _lineVertices[0] = PointInRect(rect, 0, 1);
+            _lineVertices[1] = PointInRect(rect, _graphRangeX, 1);
             Handles.color = Color.white * 0.4f;
-            for (var i = 1; i < 5; i++)
+            Handles.DrawAAPolyLine(2.0f, _lineVertices);
+
+            // Vertical lines
+            for (var i = 1; i < _graphRangeX; i++)
             {
                 _lineVertices[0] = PointInRect(rect, i, 0);
-                _lineVertices[1] = PointInRect(rect, i, 2);
+                _lineVertices[1] = PointInRect(rect, i, _graphRangeY);
                 Handles.DrawAAPolyLine(2.0f, _lineVertices);
             }
-
-                _lineVertices[0] = PointInRect(rect, 0, 1);
-                _lineVertices[1] = PointInRect(rect, 5, 1);
-                Handles.DrawAAPolyLine(2.0f, _lineVertices);
 
             // Threshold line
             _lineVertices[0] = PointInRect(rect, threshold, 0);
-            _lineVertices[1] = PointInRect(rect, threshold, 2);
-            Handles.color = Color.white * 0.8f;
+            _lineVertices[1] = PointInRect(rect, threshold, _graphRangeY);
+            Handles.color = Color.white * 0.6f;
             Handles.DrawAAPolyLine(2.0f, _lineVertices);
 
-            Handles.color = Color.white * 0.9f;
-
-            for (var i = 0; i < _curveResolution; i++)
+            // Response curve
+            var vcount = 0;
+            while(vcount < _curveResolution)
             {
-                var x = 5.0f * i / (_curveResolution - 1);
+                var x = _graphRangeX * vcount / (_curveResolution - 1);
+
                 var y1 = Mathf.Max(0, x - threshold);
-                var y2 = x - param2;
-                y2 = param1 * y2 * y2;
+                var y2 = x - coeff2;
+                y2 = coeff1 * y2 * y2;
 
                 var y = Mathf.Abs(x - threshold) < knee ? y2 : y1;
                 y *= bloom.intensity;
-                if (y > 1.5f)
-                _curveVertices[i] = _curveVertices[i - 1];
-                    else
-                _curveVertices[i] = PointInRect(rect, x, y);
+
+                _curveVertices[vcount++] = PointInRect(rect, x, y);
+                if (y > _graphRangeY) break;
             }
 
-            Handles.DrawAAPolyLine(2.0f, _curveVertices);
+            Handles.color = Color.white * 0.9f;
+            Handles.DrawAAPolyLine(2.0f, vcount, _curveVertices);
         }
 
         Vector3 PointInRect(Rect rect, float x, float y)
         {
-            x = Mathf.Lerp(rect.x, rect.xMax, x / 5);
-            y = Mathf.Lerp(rect.y, rect.yMax, 1 - y / 1.5f);
+            x = Mathf.Lerp(rect.x, rect.xMax, x / _graphRangeX);
+            y = Mathf.Lerp(rect.yMax, rect.y, y / _graphRangeY);
             return new Vector3(x, y, 1);
         }
+
+        #endregion
     }
 }
